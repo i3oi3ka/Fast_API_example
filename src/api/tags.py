a5f97b351989512f030ce4661a879c78125184ba
i3oi3ka <i3oi3ka@ukr.net>
2025-09-04 22:39:48 +0300

    feat: enhance tags and notes APIs to include user context for CRUD operations

--- src/api/tags.py
+++ src/api/tags.py
@@ -4,25 +4,33 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db import get_db
-from src.schemas import TagModel, TagResponse
+from src.schemas import TagModel, TagResponse, User
 from src.services.tags import TagService
+from src.services.auth import get_current_user
 
 router = APIRouter(prefix="/tags", tags=["tags"])
 
 
 @router.get("/", response_model=List[TagResponse])
 async def read_tags(
-    skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)
+    skip: int = 0,
+    limit: int = 100,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
 ):
     tag_service = TagService(db)
-    tags = await tag_service.get_tags(skip, limit)
+    tags = await tag_service.get_tags(skip, limit, user)
     return tags
 
 
 @router.get("/{tag_id}", response_model=TagResponse)
-async def read_tag(tag_id: int, db: AsyncSession = Depends(get_db)):
+async def read_tag(
+    tag_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
+):
     tag_service = TagService(db)
-    tag = await tag_service.get_tag(tag_id)
+    tag = await tag_service.get_tag(tag_id, user)
     print(tag)
     if tag is None:
         raise HTTPException(
@@ -32,15 +40,24 @@ async def read_tag(tag_id: int, db: AsyncSession = Depends(get_db)):
 
 
 @router.post("/", response_model=TagResponse, status_code=status.HTTP_201_CREATED)
-async def create_tag(body: TagModel, db: AsyncSession = Depends(get_db)):
+async def create_tag(
+    body: TagModel,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
+):
     tag_service = TagService(db)
-    return await tag_service.create_tag(body)
+    return await tag_service.create_tag(body, user)
 
 
 @router.put("/{tag_id}", response_model=TagResponse)
-async def update_tag(body: TagModel, tag_id: int, db: AsyncSession = Depends(get_db)):
+async def update_tag(
+    body: TagModel,
+    tag_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
+):
     tag_service = TagService(db)
-    tag = await tag_service.update_tag(tag_id, body)
+    tag = await tag_service.update_tag(tag_id, body, user)
     if tag is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found"
@@ -49,9 +66,13 @@ async def update_tag(body: TagModel, tag_id: int, db: AsyncSession = Depends(get
 
 
 @router.delete("/{tag_id}", response_model=TagResponse)
-async def remove_tag(tag_id: int, db: AsyncSession = Depends(get_db)):
+async def remove_tag(
+    tag_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user),
+):
     tag_service = TagService(db)
-    tag = await tag_service.remove_tag(tag_id)
+    tag = await tag_service.remove_tag(tag_id, user)
     if tag is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found"
